e16a782922b9186589cabf63e94df7bddc24d25f
kushagar0206 <kushagar2003@gmail.com>
2026-04-03 12:39:40 +0530

    practice array method splice() or slice()

--- terminal.js
+++ terminal.js
@@ -1,2 +1,18 @@
-//Arrays
+//Arrays 
+
+//10. splice()  -->  It is used to add or remove elements from an arry and it modifies the original array
+//splice(start, deleteCount, item1, item2, ...)
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+// console.log(arr.splice(2, 3, 14, 15 ))
+// console.log(arr) 
+
+
+//11. slice()  --> slice cuts a piece from and array. It creates a new array not modifies the original array
+// let arr = [1, 2, 3, 4, 5]
+// console.log(arr.slice(3))        //Starting length
+// console.log(arr.slice(1, 3))    //Starting or Ending length
+// console.log(arr)
+
+
+//12. reverse() -->  
 
